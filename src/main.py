f8ce0fc0385dbf9edf18bc255098ac8a6ef16cbb
Erik Olivero <ekolivero@pm.me>
2025-04-27 15:54:52 +0200

    headless

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from langchain_openai import ChatOpenAI
-from browser_use import Agent
+from browser_use import Agent, BrowserConfig, Browser
 import asyncio
 from dotenv import load_dotenv
 import os
@@ -9,8 +9,15 @@ from lmnr import Laminar
 # this line auto-instruments Browser Use and any browser you use (local or remote)
 Laminar.initialize(project_api_key=os.getenv("LMNR_PROJECT_API_KEY"))
 
+config = BrowserConfig(
+    headless=True
+)
+
+browser = Browser(config=config)
+
 async def main():
     agent = Agent(
+        browser=browser,
         task="Compare the price of gpt-4o and DeepSeek-V3",
         llm=ChatOpenAI(model="gpt-4o"),
     )
